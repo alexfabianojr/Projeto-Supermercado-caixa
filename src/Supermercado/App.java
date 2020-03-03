3c519c248cd0124197e4bb3e69c9c222ded8e7b7
alexfabianojr <alexfabianojr@gmail.com>
2020-03-02 22:13:21 -0300

    3 commit

---
+++ src/Supermercado/App.java
@@ -0,0 +1,9 @@
+package Supermercado;
+
+public class App {
+
+	public static void main(String[] args) {
+		Application object_toApplication = new Application ();
+		object_toApplication.startApplication();
+	}
+}
